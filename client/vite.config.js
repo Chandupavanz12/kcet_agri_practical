eb62345bdd0fc3d42ef4b0f96f3d81366a9139f7
Chandu <chandupavanz12@gmail.com>
2026-02-04 22:03:50 +0530

    Fix Vite build and generate dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    strictPort: false,
-  },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: 'dist'
   }
 })
